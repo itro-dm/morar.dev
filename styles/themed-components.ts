3dbfa8eda7dd09ae59cbe60be13c54cf738be680
itro-dm <dmitr.morar@gmail.com>
2020-03-17 15:14:49 +0200

    remove unused code

--- styles/themed-components.ts
+++ styles/themed-components.ts
@@ -1,15 +1,5 @@
-import React from 'react';
 import * as styledComponents from 'styled-components';
 import { Theme } from './theme';
-export type DeviceSize = 'phone' | 'tablet' | 'desktop' | 'ssr';
-
-type StyledFunction<T> = styledComponents.ThemedStyledFunction<any, Theme>;
-
-function withProps<T, U extends HTMLElement = HTMLElement>(
-  styledFunction: StyledFunction<React.HTMLProps<U>>
-): StyledFunction<T & React.HTMLProps<U>> {
-  return styledFunction;
-}
 
 const {
   default: styled,
@@ -21,14 +11,6 @@ const {
   ThemeConsumer,
 } = styledComponents as styledComponents.ThemedStyledComponentsModule<Theme>;
 
-export {
-  css,
-  createGlobalStyle,
-  keyframes,
-  ThemeProvider,
-  withProps,
-  ServerStyleSheet,
-  ThemeConsumer,
-};
+export { css, createGlobalStyle, keyframes, ThemeProvider, ServerStyleSheet, ThemeConsumer };
 
 export default styled;
